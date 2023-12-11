9196bc02ed1d3dae4b848f214abc51e7c66d6fdd
Kerim Satirli <k.satirli@gmail.com>
2023-12-11 21:55:38 +0100

    removes lazy-loading, fitvideo and image zoom

--- assets/js/common.js
+++ assets/js/common.js
@@ -32,62 +32,6 @@ $(document).ready(function() {
 
 
   /* =======================
-  // LazyLoad Images
-  ======================= */
-  var lazyLoadInstance = new LazyLoad({
-    elements_selector: '.lazy'
-  })
-
-
-  // =====================
-  // Ajax Load More
-  // =====================
-  var $load_posts_button = $('.load-more-posts');
-
-  $load_posts_button.click(function(e) {
-    e.preventDefault();
-    var loadMore = $('.load-more-section');
-    var request_next_link = pagination_next_url.split('/page')[0] + '/page/' + pagination_next_page_number + '/';
-
-    $.ajax({
-      url: request_next_link,
-      beforeSend: function() {
-        $load_posts_button.text('Loading...');
-      }
-    }).done(function(data) {
-      var posts = $('.grid__post', data);
-      $('.grid').append(posts);
-
-      var lazyLoadInstance = new LazyLoad({
-        elements_selector: '.lazy'
-      })
-
-      $load_posts_button.text('Load more');
-      pagination_next_page_number++;
-
-      if (pagination_next_page_number > pagination_available_pages_number) {
-        loadMore.addClass('hide');
-      }
-    });
-  });
-
-
-  /* =======================
-  // Responsive Videos
-  ======================= */
-  $(".post__content, .page__content").fitVids({
-    customSelector: ['iframe[src*="ted.com"]', 'iframe[src*="player.twitch.tv"]', 'iframe[src*="facebook.com"]']
-  });
-
-
-  /* =======================
-  // Zoom Image
-  ======================= */
-  $(".page img, .post img").attr("data-action", "zoom");
-  $(".page a img, .post a img").removeAttr("data-action", "zoom");
-
-
-  /* =======================
   // Scroll Top Button
   ======================= */
   $(".top").click(function() {
